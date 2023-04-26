925b959e7282bea2a8577954eb0abc1da2c9ad35
christian-the-brewer <brewer.christiang@gmail.com>
2023-04-25 21:20:27 -0400

    adds color change as scroll to navbar

---
+++ src/components/SliderData.jsx
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: ''
+    },
+    {
+        image: ''
+    },
+    {
+        image: ''
+    },
+    {
+        image: ''
+    },
+    {
+        image: ''
+    },
+]
